190795aaa53e10008f8b2c7dacbc3c6f26f73cec
AlejandraRoc <alejandra_rocha@outlook.es>
2022-04-17 15:10:09 -0500

    Reto 1 Sprint 2

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+const styledContainer = styled.div`
+    background-color: hsl(180, 52%, 96%);
+
+` 
+function App() {
+  return (
+    <styledContainer>
+      
+    </styledContainer>
+  );
+}
+
+export default App;
